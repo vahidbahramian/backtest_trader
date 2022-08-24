8b279d2e7fca44cc9ce34379f657bb6602ba868e
Vahid Bahramian <vahid.bahramian92@gmail.com>
2022-08-24 17:09:32 +0430

    run optimizer

--- RunFile.py
+++ RunFile.py
@@ -4,6 +4,8 @@ from __future__ import (absolute_import, division, print_function,
 import datetime  # For datetime objects
 import os.path  # To manage paths
 import random
+import re
+import datefinder
 import sys  # To find out the script name (in argv[0])
 
 # Import the backtrader platform
@@ -13,84 +15,101 @@ import time
 
 import backtrader as bt
 import matplotlib.pyplot
+import pandas as pd
 
 import quantstats
 import pyfolio as pf
 
+def get_date(s_date):
+    match = re.findall(r'\d{4}-\d{2}-\d{2}', s_date)
+    return match
+
+def get_asset(s_date):
+    match = re.findall("(?<=_)(.*?)(?=USDT_)", s_date)[0].split("_")[1]
+    return match
+
 def Run(strategy, out):
-    # Create a cerebro entity
-    cerebro = bt.Cerebro()
-
-    # Add a strategy
-    cerebro.addstrategy(strategy)
-    # cerebro.addstrategy(strat.BuyAndHold)
-
     # Datas are in a subfolder of the samples. Need to find where the script is
     # because it could have been called from anywhere
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-    # datapath = os.path.join(modpath, 'data\\Binance_BTCUSDT_4h_2021-01-01 00-00-00_2022-06-22 16-00-00.csv')
-    datapath = os.path.join(modpath, 'data\\Binance_ADAUSDT_1h_2021-01-01 00-00-00_2022-07-24 23-00-00.csv')
-    # datapath1 = os.path.join(modpath, 'data\\Binance_BTCUSDT_1m_2021-01-01 00-00-00_2022-06-26 14-30-00.csv')
-    # Create a Data Feed
-    # data = bt.feeds.YahooFinanceCSVData(
-    #     dataname=datapath,
-    #     # Do not pass values before this date
-    #     fromdate=datetime.datetime(2000, 1, 1),
-    #     # Do not pass values before this date
-    #     todate=datetime.datetime(2000, 12, 31),
-    #     # Do not pass values after this date
-    #     reverse=False)
-    data = bt.feeds.GenericCSVData(dataname=datapath, timeframe=bt.TimeFrame.Minutes, compression=60)
-    # data1 = bt.feeds.GenericCSVData(dataname=datapath1, timeframe=bt.TimeFrame.Minutes, compression=60)
-    # Add the Data Feed to Cerebro
-    # cerebro.adddata(data1)
-    cerebro.adddata(data)
-    # cerebro.resampledata(data, timeframe=bt.TimeFrame.Minutes, compression=60)
-    # cerebro.adddata(data)
+    data_path = os.path.join(modpath, 'data\\Binance_BNBUSDT_1h_2018-01-01 00-00-00_2022-07-31 23-00-00.csv')
+    results_data['Currency'] = get_asset(data_path)
+    range_dates = get_date(data_path)
+    dates = pd.date_range(start=range_dates[0], end=range_dates[1], freq=pd.offsets.MonthBegin(1))
+    for i in range(len(dates)-1):
+        data = bt.feeds.GenericCSVData(dataname=data_path, timeframe=bt.TimeFrame.Minutes, compression=60,
+                                       fromdate=datetime.datetime.utcfromtimestamp(dates.values[i].tolist() / 1e9),
+                                       todate=datetime.datetime.utcfromtimestamp(dates.values[i+1].tolist() / 1e9))
 
-    # Set our desired cash start
-    cerebro.broker.setcash(10000.0)
+        # Create a cerebro entity
+        # cerebro = bt.Cerebro()
+        cerebro = bt.Cerebro(maxcpus=4, runonce=False, optreturn=True)
 
-    # Add a FixedSize sizer according to the stake
-    cerebro.addsizer(bt.sizers.PercentSizer, percents=98)
+        # Add a strategy
+        # cerebro.addstrategy(strategy)
+        cerebro.optstrategy(strategy, maperiod=range(20, 25))
 
-    # Set the commission
-    cerebro.broker.setcommission(commission=0.001)
-    cerebro.addwriter(bt.WriterFile, csv=True, out="1.csv")
-    # Print out the starting conditions
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        # Add the Data Feed to Cerebro
+        cerebro.adddata(data)
+        # cerebro.resampledata(data, timeframe=bt.TimeFrame.Minutes, compression=60)
 
-    # Run over everything
-    cerebro.addanalyzer(bt.analyzers.PyFolio, _name='pyfolio')
-    cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='mysharpe')
-    cerebro.addanalyzer(bt.analyzers.Returns, _name='myreturns')
-    cerebro.addanalyzer(bt.analyzers.DrawDown, _name='mydrowdown')
-    results = cerebro.run(tradehistory=True, timeframe=bt.TimeFrame.Minutes, compression=60)
-    res = results[0]
-    print('Sharpe Ratio:', res.analyzers.mysharpe.get_analysis())
-    print('Returns:', res.analyzers.myreturns.get_analysis())
-    print('DrowDown:', res.analyzers.mydrowdown.get_analysis())
-    pyfoliozer = res.analyzers.getbyname('pyfolio')
-    returns, positions, transactions, gross_lev = pyfoliozer.get_pf_items()
-    # ax = pf.plot_returns(returns)
-    # ax.plot()
-    # fig = pf.create_position_tear_sheet(returns, positions, return_fig=True)
-    # fig = pf.create_txn_tear_sheet(returns, positions, transactions, return_fig=True)
-    # pf.create_full_tear_sheet(
-    #     returns,
-    #     positions=positions,
-    #     transactions=transactions,
-    #     live_start_date='2021-05-01',
-    #     round_trips=True)
+        # Set our desired cash start
+        cerebro.broker.setcash(10000.0)
 
-    # returns.index = returns.index.tz_convert(None)
-    # quantstats.reports.html(returns, output='returns.html')
+        # Add a FixedSize sizer according to the stake
+        cerebro.addsizer(bt.sizers.PercentSizer, percents=98)
 
-    # Print out the final result
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        # Set the commission
+        cerebro.broker.setcommission(commission=0.001)
 
-    # Plot the result
-    cerebro.plot()
-    # quantstats.plots.snapshot(positions, title='Facebook Performance')
 
-    # matplotlib.pyplot.show()
+        # cerebro.addwriter(bt.WriterFile, csv=True, out="1.csv")
+
+        # Print out the starting conditions
+        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+        # Add analysis to cerebro
+        cerebro.addanalyzer(bt.analyzers.PyFolio, _name='pyfolio')
+        cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='mysharpe', timeframe=bt.TimeFrame.Minutes, compression=60)
+        cerebro.addanalyzer(bt.analyzers.Returns, _name='myreturns')
+        cerebro.addanalyzer(bt.analyzers.DrawDown, _name='mydrowdown')
+
+        results = cerebro.run(timeframe=bt.TimeFrame.Minutes, compression=60)
+        print('==================================================')
+        for res in results:
+            print(res)
+            for strat in res:
+                print('Sharpe Ratio:', strat.analyzers.mysharpe.get_analysis())
+                results_data['Sharpe Ratio'] = strat.analyzers.mysharpe.get_analysis()['sharperatio']
+                print(strat.p._getkwargs()['maperiod'])
+                print('--------------------------------------------------')
+        print('==================================================')
+
+        # res = results[0]
+        # print('Sharpe Ratio:', res.analyzers.mysharpe.get_analysis())
+        # print('Returns:', res.analyzers.myreturns.get_analysis())
+        # print('DrowDown:', res.analyzers.mydrowdown.get_analysis())
+        # pyfoliozer = res.analyzers.getbyname('pyfolio')
+        # returns, positions, transactions, gross_lev = pyfoliozer.get_pf_items()
+        # ax = pf.plot_returns(returns)
+        # ax.plot()
+        # fig = pf.create_position_tear_sheet(returns, positions, return_fig=True)
+        # fig = pf.create_txn_tear_sheet(returns, positions, transactions, return_fig=True)
+        # pf.create_full_tear_sheet(
+        #     returns,
+        #     positions=positions,
+        #     transactions=transactions,
+        #     live_start_date='2021-05-01',
+        #     round_trips=True)
+
+        # returns.index = returns.index.tz_convert(None)
+        # quantstats.reports.html(returns, output='returns.html')
+
+        # Print out the final result
+        print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+        # Plot the result
+        # cerebro.plot()
+        # quantstats.plots.snapshot(positions, title='Facebook Performance')
+
+        # matplotlib.pyplot.show()
--- main.py
+++ main.py
@@ -4,16 +4,14 @@ from multiprocessing import Process, Queue
 from RunFile import Run
 
 if __name__ == '__main__':
-    strategys = [strat.BuyAndHold, strat.Strategy_2]
+    strategys = [strat.Strategy_2]
+
+    # for strategy in strategys:
+    #     output = Queue()
+    #     p = Process(target=Run, args=(strategy, output,))
+    #     p.start()
+    # p.join()
 
     for strategy in strategys:
         output = Queue()
-        p = Process(target=Run, args=(strategy, output,))
-        p.start()
-    p.join()
-
-        # try:
-        #     th = threading.Thread(target=Run, args=(strategy,len(strategys),))
-        #     th.start()
-        # except:
-        #     print("Error: unable to start thread")
+        p = Run(strategy, output)
